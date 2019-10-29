6815ef6109195783f4eec4813fe450df9923c605
jckent <chasekent30@gmail.com>
2019-10-29 19:59:55 -0400

    added file paths for nav

--- openTable.js
+++ openTable.js
@@ -2,11 +2,12 @@ var path = require('path');
 
 module.exports = function(app) {
 
-  app.get("/tables", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/view.html"));
   });
 
+  
   app.get("/reserve", function(req, res){
-    res.sendFile(path.join(__dirname, ""))
+    res.sendFile(path.join(__dirname, "../public/add.html"))
   })
 }
