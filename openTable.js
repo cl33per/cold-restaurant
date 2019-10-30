426aec54067940e4aa1b7d70a02e0d9e3399ecd3
jckent <chasekent30@gmail.com>
2019-10-29 20:36:39 -0400

    fixed server path names

--- openTable.js
+++ openTable.js
@@ -1,13 +1,13 @@
 var path = require('path');
 
+
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/view.html"));
+    res.sendFile(path.join(__dirname, "/public/view.html"));
   });
-
   
   app.get("/reserve", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/add.html"))
+    res.sendFile(path.join(__dirname, "/public/add.html"))
   })
 }
--- server.js
+++ server.js
@@ -4,14 +4,15 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-app.use(express.urlenclosed({
-  extended: true
-}));
+app.use(express.static('public'));
+
+//app.use(express.urlencoded({
+  //extended: true
+//}));
 app.use(express.json());
 
 
-require("./routes/apiRoutes")(app);
-require(".routes/htmlRoutes")(app);
+require("./openTable.js")(app);
 
 app.listen(PORT, function (){
   console.log("App is listening on PORT: " + PORT)
