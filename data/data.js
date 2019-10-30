28328fbc4811fd71165e36a69376d85348ca7fa3
jckent <chasekent30@gmail.com>
2019-10-29 21:29:37 -0400

    messed a bunch of stuff up

---
+++ data/data.js
@@ -0,0 +1,12 @@
+var coldRes = [
+    {
+        name: "Rocket Man",
+        time: "22:00",
+        phone: "800-000-0000",
+        partySize: 4
+    }
+
+];
+var waitRes = [];
+
+module.export = (coldRes, waitRes);
--- openTable.js
+++ openTable.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var coldRes = require('./data/data');
 
 
 module.exports = function(app) {
@@ -10,4 +11,20 @@ module.exports = function(app) {
   app.get("/add", function(req, res){
     res.sendFile(path.join(__dirname, "/public/add.html"))
   })
+
+  app.get('api/tables',function(re,res){
+    res.json(coldRes)
+  });
+
+  app.get('api/waiting',function(re,res){
+    res.json(waitRes)
+  });
+
+  app.post('api/tables'), function(req, res){
+    if(coldRes.length<5){
+      coldRes.push(req.body)
+      res.json(true);
+    }else{waitRes.push(req.body)
+    res.json(false)}
+  };
 }
